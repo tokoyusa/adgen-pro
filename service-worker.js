44238d3a5684f6af4b75d84d6f00ef8c9124c95a
tokoyusa <81419433+tokoyusa@users.noreply.github.com>
2026-05-25 19:18:20 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "adgen-pro-v2";
+const CACHE_NAME = "adgen-pro-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -6,30 +6,29 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function(event) {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener("fetch", function(event) {
+  const requestUrl = new URL(even
